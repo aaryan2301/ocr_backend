58292766bc77f2da8b3ef51bc76b3eb7a79a96d0
aaryan2301 <20uec035@lnmiit.ac.in>
2023-12-25 20:51:31 +0530

    final commit

--- controllers/recordController.js
+++ controllers/recordController.js
@@ -25,12 +25,42 @@ const displayRecord= asyncHandler(async (req,res)=>{
     res.status(200).json(records);
 }); 
 
-const updateRecord= asyncHandler(async (req,res)=>{
-    
-});
+const filterRecord = asyncHandler(async (req, res) => {
+    const filterCriteria  = req.params.type;
+    try {
+      let filteredRecords;
+  
+      if (filterCriteria === "name") {
+        filteredRecords = await Record.find().sort({first_name: 1});
+      } else if (filterCriteria === "date_of_issue") {
+        filteredRecords = await Record.find({}).sort({ date_of_issue: 1 });
+      } else if (filterCriteria === "date_of_expiry") {
+        filteredRecords = await Record.find({}).sort({ date_of_expiry: 1 });
+      } else if (filterCriteria === "status") {
+        filteredRecords = await Record.find({ status: "success" });
+      }
+  
+      res.status(200).json(filteredRecords);
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
+  
 
 const deleteRecord= asyncHandler(async (req,res)=>{
-    
+    const { id } = req.params; // Assuming the customId is passed as a parameter in the URL
+  
+  try {
+    const deletedRecord = await Record.findOneAndDelete({ customId: id });
+
+    if (!deletedRecord) {
+      return res.status(404).json({ message: "Record not found" });
+    }
+
+    res.status(200).json({ message: "Record deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
-module.exports={createRecord, displayRecord, updateRecord, deleteRecord};
+module.exports={createRecord, displayRecord, filterRecord, deleteRecord};
--- routes/recordRoutes.js
+++ routes/recordRoutes.js
@@ -1,11 +1,11 @@
 const express= require("express");
 const router= express.Router();
 
-const {createRecord, displayRecord, updateRecord, deleteRecord}= require("../controllers/recordController");
+const {createRecord, displayRecord, filterRecord, deleteRecord}= require("../controllers/recordController");
 
 router.route("/create").post(createRecord);
 router.route("/display").get(displayRecord);
-router.route("/update/:id").post(updateRecord);
+router.route("/filter/:type").post(filterRecord);
 router.route("/delete/:id").delete(deleteRecord);
 
 module.exports=router;
